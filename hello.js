04427ddd95dd549bb8533c253633eefe4d414089
MaheshChuahan <mahesh12041156@gmail.com>
2025-11-04 13:22:26 +0530

    add

--- hello.js
+++ hello.js
@@ -1,3 +1,18 @@
-console.log("hey there i am js ");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); // Loads .env file contents into process.env
+
+const app = express();
+
+const PORT = 5000;
 
 
+app.get("/", (req, res) => {
+  res.send("Render Node.js Server Running Successfully!");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+ 
+});
